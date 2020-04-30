2e18cb8392cc38ee4a85b4b7fe16034ce9551b44
archiemad <archiemad@hotmail.com>
2020-04-30 14:22:55 +0200

    fix catch

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -97,6 +97,7 @@ class SortieController extends AbstractController
             fclose($fp);
         }   catch (Exception $e){
             $this->addFlash("danger","erreur! veuillez vous rapprocher du service informatique");
+            $this->redirectToRoute('home');
         }
 
         //tester les données
