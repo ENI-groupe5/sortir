70d841f3cbd4e90abcdeb5ba8dbd952f983232e3
archiemad <archiemad@hotmail.com>
2020-05-07 10:15:55 +0200

    modif message assert

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -36,7 +36,7 @@ class Participant implements UserInterface, \Serializable
      */
     private $nom;
     /**
-     * @Assert\NotBlank(message="le prénom est obligatorie")
+     * @Assert\NotBlank(message="le prénom est obligatoire")
      * @Assert\Length(min="1",max="50",minMessage="le prénom doit comporter minimum un caractère",maxMessage="le prénom doit comporter maximum 50 caractères")
      * @ORM\Column(type="string", length=50)
      */
